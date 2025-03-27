0d1f69f29a60c87af23b4e06d6c6801642d3395a
vrajpatel26 <vraj.26112004@gmail.com>
2025-03-27 20:42:56 +0530

    add

--- practicals.py
+++ practicals.py
@@ -168,7 +168,125 @@ print(f"sum of {n} is :",sum)
 # sum of 10 is : 45
 
 """
-# lll
-# lslslsl
 
 
+# 2.2 Write a Python program to print Fibonacci series up to n terms. 
+"""
+num = 10
+n1, n2 = 0, 1
+print("Fibonacci Series:", n1, n2, end=" ")
+for i in range(2, num):
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    print(n3, end=" ")
+"""
+
+#3.3 WAP to find the reverse of given numbers (Example2564-4652).
+"""
+num = 2564
+reverse = 0
+while num > 0:
+    remainder = num % 10
+    reverse = (reverse * 10) + remainder
+    num = num // 10
+
+print(reverse)
+
+"""
+# 3.4 WAP to check whether entered number is prime or not. 
+
+"""
+num = int(input("Enter a number: "))
+
+
+if num <= 1:
+    print(f"{num} is not a prime number.")
+elif num == 2:
+    print(f"{num} is a prime number.")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print(f"{num} is not a prime number.")
+            break
+    else:
+        print(f"{num} is a prime number.")
+
+"""
+
+#3.5 WAP to print all eve numbers between 1 to n except the numbers Divisible by 6. 
+"""
+num = int(input("Enter number : "))
+
+for i in range(1,num+1):
+    if(i % 6 == 0):
+        continue
+    print(i)
+
+  """  
+
+
+#6.6 Write a python program to check whether given number is Armstrong or not. 
+"""
+num = int(input("Enter a number: "))
+
+num_digits = len(str(num))
+
+sum_of_digits = sum(int(digit) ** num_digits for digit in str(num))
+
+if sum_of_digits == num:
+    print(f"{num} is an Armstrong number.")
+else:
+    print(f"{num} is not an Armstrong number.")
+
+"""
+
+#3.7 Write a python program to check whether given number is Palindrome or not. 
+"""
+num = int(input("Enter a number: "))
+
+# Convert the number to a string and reverse it
+reverse_num = str(num)[::-1]
+
+if str(num) == reverse_num:
+    print(f"{num} is a Palindrome number.")
+else:
+    print(f"{num} is not a Palindrome number.")
+
+"""
+
+
+#3.8 
+
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+
+"""# Number of rows
+n = 5
+
+# Loop to print the pattern
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()  # Move to the next line
+"""
+
+
+# *****
+# ****
+# ***
+# **
+# *
+
+
+n = 5
+
+# Loop to print the pattern
+for i in range(n, 0, -1):  #Outer loop starts from n and decreases to 1.
+    for j in range(i):
+        print("*", end="")
+    print()  # Move to the next line
