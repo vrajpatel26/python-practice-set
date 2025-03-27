1d352fa805c9622f31c1940e5e0f0e11d856a0e9
vrajpatel26 <vraj.26112004@gmail.com>
2025-03-27 16:16:24 +0530

    Create practices.py

---
+++ practices.py
@@ -0,0 +1,173 @@
+
+# a = "3.1"
+# b = float(a)
+# t = type(b)
+# print(t)
+
+
+
+# a = int(input("Enter no 1 : "))
+
+# b = int(input("ENter no 2 : "))
+
+# print("sum is : ",a+b)
+
+
+
+# a = int(input("enter number"))
+
+# print("square is : " ,a**2)
+
+"""
+print('''hello
+      i am 
+      vraj
+      patel
+      ''')"""
+
+
+
+# name = "vraj_patel"
+
+# nameshort = name[0:5]
+
+# print(nameshort)
+
+
+
+# a = "abcdefghijklmnopqrstuvwxyz"
+
+# print(a[1:9:5])
+
+# b = "0123456789"
+
+# print(b[1:7:4])
+
+
+
+
+# name = input("Enter your name : ")
+
+# print(f"good Aftrernoon , {name}")
+
+# print("good Aftrernoon "+name+"  ")
+
+
+
+# letter = '''Dear <|Name|>
+# You are selected !
+# <|Date|> '''
+
+# print(letter.replace("<|Name|>","Vraj").replace("<|Date|>","20-01-2020"))
+
+
+
+
+# l1 = [1 , 3, 5 , 7 , 8 , 234 , 56]
+
+# l1.remove(234)
+
+# print(l1)
+
+
+
+
+# a = {
+#       "vraj" : 100,
+#       "manav" : 10,
+#       "hardik" : 33,
+#       "List" : [1,23,3]
+# }
+
+# a.update({"hardik" : 28}) 
+
+# new = a.pop("hardik")
+# print(new)
+
+# a.popitem()
+# print(a)
+
+# print(type(50/3))
+
+
+
+
+
+# l = [1,4,6,7,9,"hello"]
+
+# print(l)
+
+# S = {1,3,4,5,6,6,"hello",6,7,4,"hello"}
+
+
+# # S.add("vraj")
+# # S.clear()
+
+# print(S)
+
+
+
+
+
+# S = {1,2,3,4,"hh"}
+
+# # S.add(77)
+
+
+
+
+# for i in range(50):
+#       if(i==30):
+#             break
+#       print(i)
+
+# for i in range(50):
+#        if(i==30):
+#              continue
+#        print(i)
+
+# for i in range(50): 
+#       pass
+
+# i = 0
+# while(i<45):
+#       print(i)
+#       i+=1
+
+
+
+
+# class Employee:
+#       salary = 120000
+#       Language = "python"
+
+#       def get_info(self):
+#             print(f"langauge is {self.Language}")
+
+# vraj = Employee()
+# vraj.Language = "java"
+# print(vraj.salary , vraj.Language)
+
+# vraj.get_info()
+
+
+
+
+
+# n = int(input("enter nu"))
+
+# rev = 0
+
+# while n > 0:
+#       digit = n % 10
+#       rev = rev*10 + digit
+#       n = n // 10
+
+# print(rev)
+
+
+# L = [[3,0]*3]*3
+# L[1]="hi"
+
+# print(L)
+
